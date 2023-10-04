b06c95e063dda1ebad23034780888d1158f7681c
Sávyo Gabriel <112639440+IamGabrielHz@users.noreply.github.com>
2023-10-04 17:04:00 -0300

    Update task_models.py

--- tasks-api/persistence/task_models.py
+++ tasks-api/persistence/task_models.py
@@ -21,4 +21,43 @@ class User(UserBase, table=True):
         user.login = user_create.login
         user.name = user_create.name
         user.password = user_create.password
-        return user 
+        return user
+
+    def to_user_read(self):
+        user = UserRead(id=self.id, name=self.name, login=self.login)
+        return user
+
+
+class UserRead(UserBase):
+    id: str
+
+
+class TaskBase(SQLModel):
+    description: str = Field(min_length=3)
+    done: bool = Field(default=False)
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class Task(TaskBase, table=True):
+    id: str = Field(default=ulid(), primary_key=True)
+    user_id: str | None = Field(default=None, foreign_key="user.id")
+
+    @staticmethod
+    def from_task_create(task_create: TaskCreate):
+        task = Task()
+        task.description = task_create.description
+        task.done = task_create.done
+        return task
+
+    def to_task_read(self):
+        task = TaskRead(
+            id=self.id, description=self.description, done=self.done)
+        return task
+
+
+class TaskRead(TaskBase):
+    id: str
+    owner: UserRead | None = None
