e3d2a017ad66e2a28fd8ab5b5a0d46089bad939f
Sávyo Gabriel <112639440+IamGabrielHz@users.noreply.github.com>
2023-10-04 19:53:35 -0300

    Update db_utils.py

--- tasks-api/persistence/db_utils.py
+++ tasks-api/persistence/db_utils.py
@@ -2,7 +2,8 @@ from sqlmodel import create_engine, SQLModel
 
 
 def get_engine():
-    url = 'sqlite:///database.db'
+    #url = 'sqlite:///database.db'
+    url = 'postgresql+psycopg2://tasks_api_user:B0cvydl1RsTc8107sMeIVoz9niqgJIj0@dpg-ckerlgvs0fgc73c55hgg-a.oregon-postgres.render.com/tasks_api'
     return create_engine(url, echo=True)
 
 
